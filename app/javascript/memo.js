ac2f43cd3ae36ed4230304a6b50fac172d0b3798
gogonikkei <hahquqqhoaoz7wa@gmail.com>
2024-10-17 18:20:28 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
 function post (){
   const form = document.getElementById("form");
    //リクエストを送信する処理、
@@ -28,10 +42,34 @@ form.addEventListener("submit", (e) => {
 
 // 第一引数にはHTTPメソッド、第二引数にはパス、
 // 第三引数には非同期通信であるかをtrueかfalseで記述
-XHR.responseType = "json";
-XHR.send(formData);
+    XHR.open("POST", "/posts", true);  //「post」→「posts」に戻す
+    XHR.responseType = "json";
+    XHR.send(formData);
 // send()メソッドとは、リクエストを送信するメソッドです。
 // XMLHttpRequestオブジェクトのメソッドの一種
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");     
+// リセットの対象となるフォームの要素contentを取得して、変数formTextに格納
+      const item = XHR.response.post;
+      const html = `
+        <div class="post">
+          <div class="post-date">
+            投稿日時：${item.created_at}
+          </div>
+          <div class="post-content">＄
+            ${item.content}
+          </div>
+        </div>`;
+        list.insertAdjacentHTML("afterend", html);
+        formText.value = "";
+//formTextのvalue属性に空の文字列を指定することで、フォームの
+//中身をリセットしている
+  };
 };
  
  window.addEventListener('turbo:load', post);
